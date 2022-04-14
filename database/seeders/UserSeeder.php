8e7d81e17a275322d7ae96dab5ab527d1760a9bc
Pezu-git <artem_nikolaev69@mail.ru>
2022-04-14 19:56:28 +0300

    third

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
   {
     DB::table('users')->insert([
       'name' => 'Uve',
-      'remember_token' => Str::random(20),
+      'remember_token' => 'hFgh4k:Lk533fgdkj64657',
     ]);
   }
 }
