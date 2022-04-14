4414e147a12127414b2d8c8fa3bf879c1bfd38ff
Pezu-git <artem_nikolaev69@mail.ru>
2022-04-14 19:00:41 +0300

    third

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\CategoryController;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ApiTokenController;
+use App\Http\Controllers\CategoryController;
+
+
+
+Route::middleware('auth_api')->get('/user/{id}', function (Request $request, $id) {
+    $user = \App\Models\User::find($id);
+    if (!$user) return response('', 404);
+    return $user;
+});
+
 
 Route::group(
     ['middleware' => 'auth_api'],
     function () {
-
         Route::get('/user',  function () {
             $user = \App\Models\User::all();
             return $user;
         });
-
 
         Route::get(
             '/token/create',
@@ -24,16 +29,10 @@ Route::group(
     }
 );
 
+Route::get('/product',  [ProductController::class, 'getAllProduct']);
+Route::get('/product/{id}',  [ProductController::class, 'productCategories']);
+Route::post('/product/sortByCat',  [ProductController::class, 'sortByCatId']);
+Route::post('/product/sortByPrice',  [ProductController::class, 'sortByPrice']);
 
-
-
-Route::get(
-    '/token/create',
-    [\App\Http\Controllers\ApiTokenController::class, 'createToken']
-);
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/product',  [ProductController::class, 'getAllProduct'])->name('index');
+Route::get('/category',  [CategoryController::class, 'getAllCategory']);
+Route::get('/category/{id}',  [CategoryController::class, 'productCategories']);
